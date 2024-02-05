5bb463d43b08a1818ddc0fbe87fab2238771b06e
Eshika Dey <edey@uwaterloo.ca>
2024-02-05 15:25:39 -0500

    initial setup

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./home.css"
+
+const Home = () => {
+  return (
+    <>
+      <div className="home-container"> 
+        <div className="home-background">Welcome to My Data Pal</div>
+      </div>
+    </>
+  )
+}
+
+export default Home;
